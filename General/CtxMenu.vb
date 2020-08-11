66b356c93587cf65599c3de274f29b46c53b6273
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-12 00:00:15 +0200

    Added ImageHandling functions for context menu, added Context Menu ActionType, EntryInfo and entryDict

--- General/CtxMenu.vb
+++ General/CtxMenu.vb
@@ -1,3 +1,66 @@
-Public Class CtxMenu
+Imports System
+Imports System.Collections.Generic
 
+Public Class CtxMenu
+    Public Enum ActionType
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be an array with two strings</summary>
+        Launch
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be an array with two strings</summary>
+        Admin
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be an array with two strings</summary>
+        Execute
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        Show
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be an array with two strings</summary>
+        Properties
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        OpenWith
+
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        Cut
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        Copy
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        Paste
+
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        PasteAsHardlink
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        PasteAsSymlink
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        PasteAsShortcut
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be a string</summary>
+        PasteAsJunction
+
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be Nothing</summary>
+        CopyTo
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be Nothing</summary>
+        MoveTo
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be Nothing</summary>
+        DeleteToRecycleBin
+        ''' <summary><see cref="EntryInfo.ActionArgs"/> should be Nothing</summary>
+        DeletePermanently
+    End Enum
+
+    Structure EntryInfo
+        Public IconPath As String
+        Public Text As String
+        Public AdminIcon As Boolean
+
+        'when to show
+        Public Extended As Boolean
+        Public DirectoryOnly As Boolean
+        Public DriveOnly As Boolean
+        Public FileOnly As Boolean
+        ''' <summary>
+        ''' Item filters separated by ";".
+        ''' See https://docs.microsoft.com/en-us/dotnet/visual-basic/language-reference/operators/like-operator#pattern-options for filter info
+        ''' </summary>
+        Public Filter As String
+
+        Public ActionType As ActionType
+        Public ActionArgs As Object
+    End Structure
+
+    Private entryDict As New Dictionary(Of Integer, EntryInfo)
 End Class
--- General/ImageHandling.vb
+++ General/ImageHandling.vb
@@ -1,3 +1,31 @@
-Public Class ImageHandling
+Imports System
+Imports System.Drawing
+Imports System.IO
 
+Public Class ImageHandling
+    Public Shared Function TransformResourcePath(iconResource As String, Optional ByRef iconIndex As Integer = 0) As String
+        If iconResource.Contains(",") Then
+            If Microsoft.VisualBasic.IsNumeric(iconResource.Substring(iconResource.LastIndexOf(",") + 1)) Then
+                iconIndex = Integer.Parse(iconResource.Substring(iconResource.LastIndexOf(",") + 1))
+
+                iconResource = iconResource.Remove(iconResource.LastIndexOf(","))
+            End If
+        End If
+
+        Return iconResource
+    End Function
+
+    Public Shared Function GetIcon(resourcePath As String) As Icon
+        If resourcePath Is Nothing Then Return Nothing
+
+        Dim iconIndex As Integer = 0
+        resourcePath = TransformResourcePath(resourcePath, iconIndex)
+        resourcePath = Environment.ExpandEnvironmentVariables(resourcePath)
+
+        If File.Exists(resourcePath) Then
+            Return WalkmanLib.ExtractIconByIndex(resourcePath, iconIndex, 16)
+        Else
+            Return Nothing
+        End If
+    End Function
 End Class
