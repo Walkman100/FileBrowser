fd5b8503685c6d1f0a64db6e3982126157b9c5f2
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-14 22:08:34 +0200

    added subs to ContextMenuConfig.vb

--- Forms/ContextMenuConfig.vb
+++ Forms/ContextMenuConfig.vb
@@ -1,3 +1,77 @@
-Public Class ContextMenuConfig
+Imports System.Windows.Forms
 
+Public Class ContextMenuConfig
+    Private Sub lstMain_SelectedIndexChanged() Handles lstMain.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnAdd_Click() Handles btnAdd.Click
+
+    End Sub
+
+    Private Sub btnDelete_Click() Handles btnDelete.Click
+
+    End Sub
+
+    Private Sub btnMoveUp_Click() Handles btnMoveUp.Click
+
+    End Sub
+
+    Private Sub btnMoveDown_Click() Handles btnMoveDown.Click
+
+    End Sub
+
+    Private Sub btnSave_Click() Handles btnSave.Click
+
+    End Sub
+
+    Private Sub btnCancel_Click() Handles btnCancel.Click
+
+    End Sub
+
+    Private Sub btnShowConfig_Click() Handles btnShowConfig.Click
+
+    End Sub
+
+
+
+    Private Sub cbxItemType_SelectedIndexChanged() Handles cbxItemType.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub txtItemText_TextChanged() Handles txtItemText.TextChanged
+
+    End Sub
+
+    Private Sub chkItemAdmin_CheckedChanged() Handles chkItemAdmin.CheckedChanged
+
+    End Sub
+
+    Private Sub chkItemExtended_CheckedChanged() Handles chkItemExtended.CheckedChanged
+
+    End Sub
+
+    Private Sub chkItemRestrict_CheckedChanged() Handles chkItemRestrict.CheckedChanged
+
+    End Sub
+
+    Private Sub cbxItemRestrict_SelectedIndexChanged() Handles cbxItemRestrict.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub txtItemFilter_TextChanged() Handles txtItemFilter.TextChanged
+
+    End Sub
+
+    Private Sub cbxItemActionType_SelectedIndexChanged() Handles cbxItemActionType.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub txtItemActionFile_TextChanged() Handles txtItemActionFile.TextChanged
+
+    End Sub
+
+    Private Sub txtItemActionArgs_TextChanged() Handles txtItemActionArgs.TextChanged
+
+    End Sub
 End Class
