f0da8e13cc831e045eeaa904a66071938c47166e
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-15 20:39:31 +0200

    added helper functions, added regions

--- Forms/ContextMenuConfig.vb
+++ Forms/ContextMenuConfig.vb
@@ -1,6 +1,67 @@
 Imports System.Windows.Forms
 
 Public Class ContextMenuConfig
+#Region "Helper Functions"
+    Private Function GetFilterText(itemInfo As CtxMenu.EntryInfo) As String
+        Dim rtn As String = String.Empty
+        If (itemInfo.FileOnly AndAlso itemInfo.DirectoryOnly AndAlso itemInfo.DriveOnly) OrElse
+            (itemInfo.FileOnly AndAlso itemInfo.DirectoryOnly) OrElse
+            (itemInfo.FileOnly AndAlso itemInfo.DriveOnly) OrElse
+            (itemInfo.DirectoryOnly AndAlso itemInfo.DriveOnly) Then
+            rtn = "No Items"
+        ElseIf itemInfo.FileOnly Then
+            rtn = "Files Only"
+        ElseIf itemInfo.DirectoryOnly Then
+            rtn = "Directories Only"
+        ElseIf itemInfo.DriveOnly Then
+            rtn = "Drives Only"
+        End If
+
+        If Not String.IsNullOrEmpty(itemInfo.Filter) Then
+            If rtn = String.Empty Then
+                rtn = "Files filtered by: " & itemInfo.Filter
+            Else
+                rtn &= ", filtered by: " & itemInfo.Filter
+            End If
+        End If
+
+        Return rtn
+    End Function
+
+    Private Function GetActionSettingsText(itemInfo As CtxMenu.EntryInfo) As String
+        If String.IsNullOrEmpty(itemInfo.ActionArgs1) Then Return Nothing
+        If String.IsNullOrEmpty(itemInfo.ActionArgs2) Then Return itemInfo.ActionArgs1
+
+        Return $"Path format: ""{itemInfo.ActionArgs1}"" Arguments format: ""{itemInfo.ActionArgs2}"""
+    End Function
+
+    Private Function UpdateItem(item As ListViewItem, itemInfo As CtxMenu.EntryInfo) As ListViewItem
+        item.Tag = itemInfo
+        item.Text = itemInfo.EntryType.ToString()
+        item.SubItems.Item(1).Text = itemInfo.Text
+        item.SubItems.Item(2).Text = itemInfo.IconPath
+        item.SubItems.Item(3).Text = itemInfo.AdminIcon.ToString()
+        item.SubItems.Item(4).Text = itemInfo.Extended.ToString()
+        item.SubItems.Item(5).Text = GetFilterText(itemInfo)
+        item.SubItems.Item(6).Text = itemInfo.ActionType.ToString()
+        item.SubItems.Item(7).Text = GetActionSettingsText(itemInfo)
+        Return item
+    End Function
+
+    Private Function CreateItem(itemInfo As CtxMenu.EntryInfo) As ListViewItem
+        Return UpdateItem(New ListViewItem({"", "", "", "", "", "", "", ""}), itemInfo)
+    End Function
+
+    Private Function GetItemInfo(item As ListViewItem) As CtxMenu.EntryInfo
+        Return DirectCast(item.Tag, CtxMenu.EntryInfo)
+    End Function
+#End Region
+
+#Region "Item / Window Management"
+    Private Sub WndShown() Handles Me.Shown
+
+    End Sub
+
     Private Sub lstMain_SelectedIndexChanged() Handles lstMain.SelectedIndexChanged
 
     End Sub
@@ -32,9 +93,9 @@ Public Class ContextMenuConfig
     Private Sub btnShowConfig_Click() Handles btnShowConfig.Click
 
     End Sub
+#End Region
 
-
-
+#Region "Editing Data"
     Private Sub cbxItemType_SelectedIndexChanged() Handles cbxItemType.SelectedIndexChanged
 
     End Sub
@@ -74,4 +135,5 @@ Public Class ContextMenuConfig
     Private Sub txtItemActionArgs_TextChanged() Handles txtItemActionArgs.TextChanged
 
     End Sub
+#End Region
 End Class
