1ebb0d73b4c3f3aaf18f417a4e5349432d83616b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-10-15 00:22:28 +0200

    separated ImageHandling into modules, changed GetIcon to throw if resource file isn't found

--- General/ImageHandling.vb
+++ General/ImageHandling.vb
@@ -2,47 +2,47 @@ Imports System
 Imports System.Drawing
 Imports System.IO
 
-Public Class ImageHandling
-    Public Shared Function TransformResourcePath(iconResource As String, Optional ByRef iconIndex As Integer = 0) As String
-        If iconResource.Contains(",") Then
-            If Microsoft.VisualBasic.IsNumeric(iconResource.Substring(iconResource.LastIndexOf(",") + 1)) Then
-                iconIndex = Integer.Parse(iconResource.Substring(iconResource.LastIndexOf(",") + 1))
+Namespace ImageHandling
+    Module ImageHandling
+        Public Function TransformResourcePath(iconResource As String, Optional ByRef iconIndex As Integer = 0) As String
+            If iconResource.Contains(",") Then
+                If Microsoft.VisualBasic.IsNumeric(iconResource.Substring(iconResource.LastIndexOf(",") + 1)) Then
+                    iconIndex = Integer.Parse(iconResource.Substring(iconResource.LastIndexOf(",") + 1))
 
-                iconResource = iconResource.Remove(iconResource.LastIndexOf(","))
+                    iconResource = iconResource.Remove(iconResource.LastIndexOf(","))
+                End If
             End If
-        End If
 
-        Return iconResource
-    End Function
+            Return iconResource
+        End Function
 
-    Public Shared Function GetIcon(resourcePath As String) As Icon
-        If resourcePath Is Nothing Then Return Nothing
+        Public Function GetIcon(resourcePath As String) As Icon
+            If String.IsNullOrEmpty(resourcePath) Then Return Nothing
 
-        Dim iconIndex As Integer = 0
-        resourcePath = TransformResourcePath(resourcePath, iconIndex)
-        resourcePath = Environment.ExpandEnvironmentVariables(resourcePath)
+            Dim iconIndex As Integer = 0
+            resourcePath = TransformResourcePath(resourcePath, iconIndex)
+            resourcePath = Environment.ExpandEnvironmentVariables(resourcePath)
 
-        If File.Exists(resourcePath) Then
             Return WalkmanLib.ExtractIconByIndex(resourcePath, iconIndex, 16)
-        Else
-            Return Nothing
-        End If
-    End Function
+        End Function
+    End Module
 
-    Public Shared Function AddOverlay(image As Image, overlay As Bitmap, Optional resize As Boolean = False) As Image
-        Dim gr As Graphics = Graphics.FromImage(image)
+    Module Overlays
+        Public Function AddOverlay(image As Image, overlay As Bitmap, Optional resize As Boolean = False) As Image
+            Dim gr As Graphics = Graphics.FromImage(image)
 
-        If resize Then
-            overlay = New Bitmap(overlay, 8, 8)
-            gr.DrawImage(overlay, 8, 8, overlay.Width, overlay.Height)
-        Else
-            gr.DrawImage(overlay, 0, 0, overlay.Width, overlay.Height)
-        End If
+            If resize Then
+                overlay = New Bitmap(overlay, 8, 8)
+                gr.DrawImage(overlay, 8, 8, overlay.Width, overlay.Height)
+            Else
+                gr.DrawImage(overlay, 0, 0, overlay.Width, overlay.Height)
+            End If
 
-        Return image
-    End Function
+            Return image
+        End Function
 
-    Public Shared Function AddAdminOverlay(image As Image) As Image
-        Return AddOverlay(image, My.Resources.Resources.Admin, True)
-    End Function
-End Class
+        Public Function AddAdminOverlay(image As Image) As Image
+            Return AddOverlay(image, My.Resources.Resources.Admin, True)
+        End Function
+    End Module
+End Namespace
